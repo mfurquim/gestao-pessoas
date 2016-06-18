f48b1ad256c577b941850ccee7f5824aa9f87aad
Eduardo_Nunes <eduardonunes2525@gmail.com>
2016-06-18 18:09:14 -0300

    Add bootstrap

--- Gemfile
+++ Gemfile
@@ -13,6 +13,10 @@ gem 'uglifier', '>= 1.3.0'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
 
+# Bootstrap
+gem 'twitter-bootstrap-rails'
+gem 'devise-bootstrap-views'
+
 # Authenticate gem Devise
 gem 'devise'
 # Profile administrator
--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,7 +1,18 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-  # before_action :configure_sign_up_params, only: [:create]
-  # before_action :configure_account_update_params, only: [:update]
+# before_action :configure_sign_up_params, only: [:create]
+# before_action :configure_account_update_params, only: [:update]
 
+  # GET /resource/sign_up
+  # def new
+  #   super
+  # end
+
+  # POST /resource
+  # def create
+  #   super
+  # end
+
+  # GET /resource/edit
   # def edit
   #   super
   # end
@@ -13,7 +24,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # DELETE /resource
   # def destroy
-  #  super
+  #   super
   # end
 
   # GET /resource/cancel
@@ -27,6 +38,10 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # protected
 
+  # If you have extra params to permit, append them to the sanitizer.
+  # def configure_sign_up_params
+  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
+  # end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_account_update_params
