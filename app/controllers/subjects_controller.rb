a2d056d01b9443a7f15fcc6bb2ced966583db823
Artur Bersan de Faria <artur_bersan@hotmail.com>
2016-07-18 23:57:57 -0300

    Autorizate to subjects class

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -6,6 +6,7 @@ class SubjectsController < ApplicationController
   # GET /subjects.json
   def index
     @subjects = Subject.all
+    authorize @subjects
   end
 
   # GET /subjects/1
@@ -21,6 +22,7 @@ class SubjectsController < ApplicationController
 
   # GET /subjects/1/edit
   def edit
+    authorize @subject
   end
 
   # POST /subjects
@@ -30,11 +32,9 @@ class SubjectsController < ApplicationController
 
     respond_to do |format|
       if @subject.save
-        format.html { redirect_to @subject, notice: 'Subject was successfully created.' }
-        format.json { render :show, status: :created, location: @subject }
+        format.html { redirect_to @subject, notice: 'Disciplina cadastrada com sucesso.' }
       else
         format.html { render :new }
-        format.json { render json: @subject.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -44,8 +44,9 @@ class SubjectsController < ApplicationController
   def update
     respond_to do |format|
       if @subject.update(subject_params)
-        format.html { redirect_to @subject, notice: 'Subject was successfully updated.' }
+        format.html { redirect_to @subject, notice: 'Disciplina atualizada com sucesso!' }
         format.json { render :show, status: :ok, location: @subject }
+        format.html { render :edit }
       else
         format.html { render :edit }
         format.json { render json: @subject.errors, status: :unprocessable_entity }
@@ -58,7 +59,7 @@ class SubjectsController < ApplicationController
   def destroy
     @subject.destroy
     respond_to do |format|
-      format.html { redirect_to subjects_url, notice: 'Subject was successfully destroyed.' }
+      format.html { redirect_to subjects_url, notice: 'Disciplina removida.' }
       format.json { head :no_content }
     end
   end
@@ -72,5 +73,9 @@ class SubjectsController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def subject_params
       params.require(:subject).permit(:name)
+    end
+
+    def set_user
+      @user ||= User.find(params[:user_id])
     end
 end
