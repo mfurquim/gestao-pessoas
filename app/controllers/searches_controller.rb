1358dc546ee02a83c1f2d7bf2e0b22f468ac866b
Artur Bersan de Faria <artur_bersan@hotmail.com>
2016-10-02 17:08:25 -0300

    Class Search

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,8 +1,23 @@
 class SearchesController < ApplicationController
   def index
+    @results = PersonalInformation.all
+    search = params[:search]
+    if search
+      @results = find_user(search)
+    else
+      @results = {}
+    end
   end
 
-  def search_entities(entity_class, keyword)
-     entities = class_entity.select(:name, :id).where('name LIKE ?', "%#{keyword}%")
+  def find_user(keyword)
+    personal_information = search_user(PersonalInformation, :personal_information_id, keyword)
+    subject = search_user(Subject, :subject_id, keyword)
+    user = { information: personal_information, subject: subject }
+    return user
   end
+
+  def search_user(class_entity, name_field, keyword)
+    user = class_entity.select(:name, :id).where('name LIKE ?', "%#{keyword}%")
+  end
+
 end
