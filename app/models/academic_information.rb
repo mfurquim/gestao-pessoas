85f48fd94b364bc7a26e9c75adcdc8bc8c20b325
marcelohpf <marcelohpf@gmail.com>
2016-10-05 21:45:25 -0300

    Create association to subject and academic info
    Signed-off-by: marcelohpf <marcelohpf@gmail.com>

--- app/models/academic_information.rb
+++ app/models/academic_information.rb
@@ -1,6 +1,7 @@
 class AcademicInformation < ActiveRecord::Base
 belongs_to :user
 has_many :timetablings
+has_and_belongs_to_many :subjects
 
 validates :registration, presence: true, format:
 		  { with: /[0-9]{2}\/[0-9]{7}/ },
--- app/models/subject.rb
+++ app/models/subject.rb
@@ -2,4 +2,5 @@ class Subject < ActiveRecord::Base
   # Validations
   validates :name, format: { with: /[A-z]/, on: :create }, presence: true
   has_many :timetablings
+  has_and_belongs_to_many :academic_informations
 end
