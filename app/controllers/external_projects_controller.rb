5ea4efe17ba854b4722d305ec4cdc0451872d648
marcelohpf <marcelohpf@gmail.com>
2016-10-30 17:27:39 -0200

    Add form and methods to external project compose profile
    Signed-off-by: marcelohpf <marcelohpf@gmail.com>

--- app/controllers/external_projects_controller.rb
+++ app/controllers/external_projects_controller.rb
@@ -1,6 +1,6 @@
 class ExternalProjectsController < ApplicationController
   before_action :set_external_project, only: [:show, :edit, :update, :destroy]
-
+  before_action :set_profile
   # GET /external_projects
   # GET /external_projects.json
   def index
@@ -27,8 +27,8 @@ class ExternalProjectsController < ApplicationController
     @external_project = ExternalProject.new(external_project_params)
 
     respond_to do |format|
-      if @external_project.save
-        format.html { redirect_to @external_project, notice: 'External project was successfully created.' }
+      if @professional_profile.add_information(@external_project)
+        format.html { redirect_to [@professional_profile.user,@professional_profile,@external_project], notice: 'External project was successfully created.' }
         format.json { render :show, status: :created, location: @external_project }
       else
         format.html { render :new }
@@ -42,7 +42,7 @@ class ExternalProjectsController < ApplicationController
   def update
     respond_to do |format|
       if @external_project.update(external_project_params)
-        format.html { redirect_to @external_project, notice: 'External project was successfully updated.' }
+        format.html { redirect_to [@professional_profile.user,@professional_profile,@external_project], notice: 'External project was successfully updated.' }
         format.json { render :show, status: :ok, location: @external_project }
       else
         format.html { render :edit }
@@ -54,9 +54,9 @@ class ExternalProjectsController < ApplicationController
   # DELETE /external_projects/1
   # DELETE /external_projects/1.json
   def destroy
-    @external_project.destroy
+    @professional_profile.remove_information(@external_project)
     respond_to do |format|
-      format.html { redirect_to external_projects_url, notice: 'External project was successfully destroyed.' }
+      format.html { redirect_to [@professional_profile.user,@professional_profile], notice: 'External project was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
@@ -67,8 +67,12 @@ class ExternalProjectsController < ApplicationController
       @external_project = ExternalProject.find(params[:id])
     end
 
+    def set_profile
+      @professional_profile = ProfessionalProfile.find(params[:professional_profile_id])
+    end
+
     # Never trust parameters from the scary internet, only allow the white list through.
     def external_project_params
-      params.fetch(:external_project, {})
+      params.require(:external_project).permit(:name,:location)
     end
 end
